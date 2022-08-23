bbbe93c5cf872d1c84eb3c683a53760223454219
Monjed <monjed1010@gmail.com>
2022-08-23 12:35:01 +0300

    print in order and preorder and post order added

--- src/BST.java
+++ src/BST.java
@@ -6,6 +6,71 @@ public class BST {
         root = null;
     }
 
+    public void printInOrder(){
+        if (root!= null){
+            printInOrderRec(root);
+        }else {
+            System.out.println("Empty tree!");
+        }
+    }
+    private void printInOrderRec(Node root){
+        if (root != null){
+            printInOrderRec(root.left);
+            System.out.println(root.data);
+            printInOrderRec(root.right);
+        }
+    }
+    public void printPreOrder(){
+        if (root!= null){
+            printPreOrderRec(root);
+        }else {
+            System.out.println("Empty tree!");
+        }
+    }
+    private void printPreOrderRec(Node root){
+        if (root != null){
+            System.out.println(root.data);
+            printPreOrderRec(root.left);
+            printPreOrderRec(root.right);
+        }
+    }
+    public void printPostOrder(){
+        if (root!= null){
+            printPostOrderRec(root);
+        }else {
+            System.out.println("Empty tree!");
+        }
+    }
+    private void printPostOrderRec(Node root){
+        if (root != null){
+            printPostOrderRec(root.left);
+            printPostOrderRec(root.right);
+            System.out.println(root.data);
+        }
+    }
+
+    public void find(int key){
+        if (root == null){
+            System.out.println("Tree is empty!");
+        } else if (findRec(root, key)) {
+            System.out.println("Found!");
+        }else {
+            System.out.println("Not Found");
+        }
+    }
+    public boolean findRec(Node root, int key) {
+        if(root == null){
+            return false;
+        }
+        if (root.data == key){
+            return true;
+        } else if (root.data < key) {
+            return findRec(root.right, key);
+        }else {
+            return findRec(root.left, key);
+        }
+    }
+
     public void insert(Node node){
         if (root == null){
             root = node;
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,14 @@ public class Main {
 
         tree.insert(new Node(50));
         tree.insert(new Node(10));
+        tree.insert(new Node(2));
         tree.insert(new Node(100));
         tree.insert(new Node(70));
         tree.insert(new Node(12));
         tree.insert(new Node(18));
+
+        tree.find(187);
+//        tree.printInOrder(tree.root);
+        tree.printPostOrder();
     }
 }
